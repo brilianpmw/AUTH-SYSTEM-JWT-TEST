677a73ff8c2a6727b5bd7e2084c1332837ef899f
brilian <briliantpmw@gmai.com>
2024-07-11 15:58:51 +0700

    chore(gitignore):add .history

--- .history/internal/user/usecase/user_test_20240711151743.go
+++
@@ -1,8 +0,0 @@
-package usecase
-
-import "github.com/brilianpmw/synapsis/presentation"
-
-type MockUserUsecase struct {
-	dataGetUserDataByUserName  presentation.User
-	errorGetUserDataByUserName error
-}
